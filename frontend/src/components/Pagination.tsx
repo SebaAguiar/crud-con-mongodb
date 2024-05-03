73c0c1f619198791637119d466469d63a4233e85
SebaAguiar <sebaaguiar08@gmail.com>
2024-05-03 16:57:11 -0300

    pagination details

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -3,6 +3,8 @@ import { useStore } from '../zustand/store';
 
 const Pagination = () => {
   const pages = useStore((state) => state.totalPages);
+  const actualPage = useStore((state) => state.actualPage);
+  const setActualPage = useStore((state) => state.setActualPage);
   const setProducts = useStore((state) => state.setProducts);
   const pageNumbers = Array.from({ length: pages }, (_, i) => i + 1);
 
@@ -10,13 +12,14 @@ const Pagination = () => {
     const fetchedProducts = await fetch(`${API_URL}/products?page=${number}`);
     const json = await fetchedProducts.json();
     setProducts(json.response.products);
+    setActualPage(number);
   };
 
   return (
     <div className='w-3/6 h-14 flex justify-center'>
       {pageNumbers.map((number) => (
         <button
-          className='size-4 m-2'
+          className={`size-4 text-cente m-2 ${number === actualPage ? 'font-bold text-green' : ''}`}
           onClick={() => handlePaginationClick(number)}
           key={number}
         >
